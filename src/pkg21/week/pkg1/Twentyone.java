5a66750035a6ba6ec665189003504998e0bec190
Riley Brown <rtbrown560@outlook.com>
2013-10-03 18:23:36 -0600

    Week 1

---
+++ src/pkg21/week/pkg1/Twentyone.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg21.week.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Riley
+ */
+public class Twentyone{
+    String name;
+    String instructions = "Players have to try to get as close to twentyone\n"
+            + "as possible\n"
+            + "They can hit (recieve more cards) or stay (keep cards).";
+
+   
+    public static void main(String[] args) {
+        Twentyone myGame = new Twentyone();
+        myGame.getName();
+        myGame.displayHelp();        
+    }
+
+public void getName() {
+Scanner input = new Scanner(System.in);
+System.out.println("Enter your name: ");
+this.name = input.next();
+}
+
+
+public void displayHelp(){
+System.out.println("\nWelcome " + this.name + "\n");
+System.out.println(this.instructions);
+}
+
+
+
+
+
+}
+
